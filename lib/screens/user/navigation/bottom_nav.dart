10028ef0616f9a7ea66c43e5b2b9ac3948d1c4b0
sAyusha <ayushastha69@gmail.com>
2025-03-04 21:09:02 +0545

    added circle_nav_bar for bottom navigation

--- lib/screens/user/navigation/bottom_nav.dart
+++ lib/screens/user/navigation/bottom_nav.dart
@@ -1,6 +1,5 @@
+import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:lucide_icons/lucide_icons.dart';
 
 class BottomNavScreen extends StatefulWidget {
   const BottomNavScreen({super.key});
@@ -10,54 +9,86 @@ class BottomNavScreen extends StatefulWidget {
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 0; // Default selected index
 
   final List<Widget> _pages = [
-    const Center(child: Text("Home Page", style: TextStyle(fontSize: 22))),
-    const Center(child: Text("Search Page", style: TextStyle(fontSize: 22))),
-    const Center(child: Text("Donor Page", style: TextStyle(fontSize: 22))),
-    const Center(child: Text("Donate Page", style: TextStyle(fontSize: 22))),
-    const Center(child: Text("Updates Page", style: TextStyle(fontSize: 22))),
-    const Center(child: Text("Profile Page", style: TextStyle(fontSize: 22))),
+    const Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Updates', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Home', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Search Donor', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Donate', style: TextStyle(fontSize: 24))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: _pages[_selectedIndex],
-      bottomNavigationBar: Container(
-
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(color: Colors.black12, blurRadius: 10),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: GNav(
-            gap: 8,
-            activeColor: Colors.white,
-            iconSize: 24,
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            tabBackgroundColor: Colors.red.shade400,
-            color: Colors.black54,
-            tabs: const [
-              GButton(icon: LucideIcons.home, text: "Home"),
-              GButton(icon: LucideIcons.search, text: "Search"),
-              // GButton(icon: LucideIcons.users, text: "Donor"),
-              GButton(icon: LucideIcons.plusCircle, text: "Donate"),
-              GButton(icon: LucideIcons.bell, text: "Updates"),
-              GButton(icon: LucideIcons.user, text: "Profile"),
+      bottomNavigationBar: CircleNavBar(
+        activeIcons: const [
+          Icon(Icons.home, color: Colors.white),
+          Icon(Icons.my_location_rounded, color: Colors.white),
+          Icon(Icons.add_circle_outline, color: Colors.white),
+          Icon(Icons.notifications_active, color: Colors.white),
+          Icon(Icons.person, color: Colors.white),
+        ],
+        inactiveIcons: [
+          Column(
+            children: [
+              Icon(Icons.home_outlined, color: Colors.red.shade400),
+              const Text("Home", style: TextStyle(fontSize: 12)),
             ],
-            selectedIndex: _selectedIndex,
-            onTabChange: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
           ),
+          Column(
+            children: [
+              Icon(Icons.my_location_outlined, color: Colors.red.shade400),
+              const Text("Search",
+                  textAlign: TextAlign.center, style: TextStyle(fontSize: 12)),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.add_circle_outline, color: Colors.red.shade400),
+              const Text("Donate", style: TextStyle(fontSize: 12)),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.notifications_outlined, color: Colors.red.shade400),
+              const Text("Updates", style: TextStyle(fontSize: 12)),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.person_outline, color: Colors.red.shade400),
+              const Text("Profile", style: TextStyle(fontSize: 12)),
+            ],
+          ),
+        ],
+        color: Colors.red.shade100.withOpacity(0.8),
+        height: 60,
+        circleWidth: 60,
+
+        activeIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+        cornerRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(24),
+          bottomLeft: Radius.circular(24),
         ),
+        // shadowColor: Colors.deepPurple,
+        circleGradient: LinearGradient(
+          colors: [Colors.red.shade600, Colors.red.shade300],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        elevation: 10,
       ),
     );
   }
